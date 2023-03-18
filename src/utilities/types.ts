b085616b1cdcc764d524aeb4f6a7c434506c0bc7
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-18 11:17:47 +0500

    refactor: add types for countries data

---
+++ src/utilities/types.ts
@@ -0,0 +1,10 @@
+// makes all nested properties optional as well
+export type Subset<T> = {
+  [attr in keyof T]?: T[attr] extends object
+    ? Subset<T[attr]>
+    : T[attr] extends object | null
+    ? Subset<T[attr]> | null
+    : T[attr] extends object | null | undefined
+    ? Subset<T[attr]> | null | undefined
+    : T[attr];
+};
