2fc9857d5a2f300c8ca1ef94ce151311cb5a8053
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-25 19:44:32 +0500

    feat: add the ability to display an error message

--- src/components/form/CreateCountry/Label.tsx
+++ src/components/form/CreateCountry/Label.tsx
@@ -5,17 +5,21 @@ type LabelProps = {
   children: ReactNode;
   title: string;
   vertical?: boolean;
+  errorMessage?: string;
 };
 
 export class Label extends Component<LabelProps> {
   render() {
-    const { children, title, vertical } = this.props;
+    const { children, title, vertical, errorMessage } = this.props;
 
     return (
-      <label className={`create__label ${vertical ? 'create__label_vertical' : ''}`}>
-        <p className="create__label-title">{title}</p>
-        {children}
-      </label>
+      <div className="create__label-container">
+        <label className={`create__label ${vertical ? 'create__label_vertical' : ''}`}>
+          <p className="create__label-title">{title}</p>
+          {children}
+        </label>
+        {errorMessage ? <p className="create__label-error">{errorMessage}</p> : null}
+      </div>
     );
   }
 }
