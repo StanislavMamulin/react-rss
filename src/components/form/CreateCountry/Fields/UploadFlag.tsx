ff505bf6885ad4d69d3fb02ea831424a07a1313c
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-27 00:28:09 +0500

    feat: add ability to upload image

--- src/components/form/CreateCountry/Fields/UploadFlag.tsx
+++ src/components/form/CreateCountry/Fields/UploadFlag.tsx
@@ -1,9 +1,12 @@
-import { PureComponent, ReactNode } from 'react';
+import { createRef, PureComponent, ReactNode } from 'react';
 import { Label } from './Label';
+import { Field } from './fields.model';
 
 import '../CreateCountry.scss';
 
-export class UploadFlag extends PureComponent {
+export class UploadFlag extends PureComponent implements Field<string> {
+  uploadInput = createRef<HTMLInputElement>();
+
   render(): ReactNode {
     return (
       <Label title="Choose a country flag image:" vertical>
@@ -12,8 +15,22 @@ export class UploadFlag extends PureComponent {
           accept="image/png, image/jpeg"
           name="flag-file"
           className="flag-upload__input"
+          ref={this.uploadInput}
         ></input>
       </Label>
     );
   }
+
+  validate = (): boolean => {
+    return Boolean(this.uploadInput.current?.files);
+  };
+
+  getValue = (): string => {
+    const flagImages = this.uploadInput.current?.files;
+    if (flagImages) {
+      return URL.createObjectURL(flagImages[0]);
+    }
+
+    return '';
+  };
 }
