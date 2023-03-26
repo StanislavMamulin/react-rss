36f9a0ce037c13019793e495d204cd364e0a4b37
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-26 20:14:47 +0500

    feat: add validation to continents field

--- src/components/form/CreateCountry/Fields/Continent.tsx
+++ src/components/form/CreateCountry/Fields/Continent.tsx
@@ -1,27 +1,84 @@
-import { PureComponent, ReactNode } from 'react';
+import { PureComponent, ReactNode, RefObject, SyntheticEvent } from 'react';
 import { Label } from './Label';
 
 import '../CreateCountry.scss';
+import { Field } from './fields.model';
 
 const continents = ['Kagaria', 'Hazos', 'Abora', 'Khuntan'];
 
-export class ContinentChooser extends PureComponent {
+type ContinentProps = {
+  ref: RefObject<ContinentChooser>;
+};
+
+type ContinentState = {
+  errorMessage: string;
+};
+
+export class ContinentChooser
+  extends PureComponent<ContinentProps, ContinentState>
+  implements Field<string[]>
+{
+  state = {
+    errorMessage: '',
+  };
+  choosenContinents: string[] = [];
+
+  checkHandler = (event: SyntheticEvent) => {
+    if (!event.nativeEvent.target) return;
+
+    const checkedValue = (event.nativeEvent.target as HTMLInputElement).value;
+
+    const existContinentIndex: number = this.choosenContinents.findIndex(
+      (continent: string) => continent === checkedValue
+    );
+
+    if (existContinentIndex === -1) {
+      this.choosenContinents.push(checkedValue);
+      this.choosenContinents.sort();
+    } else {
+      this.choosenContinents.splice(existContinentIndex, 1);
+    }
+  };
+
   render(): ReactNode {
     return (
       <div className="create__continents-container">
-        <Label title="Located on the continents:" vertical>
-          {continents.map((continent: string) => (
-            <Label title={continent} key={continent}>
-              <input
-                type="checkbox"
-                name={`continent-${continent}`}
-                className="create__checkbox"
-                value={continent}
-              />
-            </Label>
-          ))}
-        </Label>
+        <p>Located on the continents:</p>
+        {continents.map((continent: string) => (
+          <Label title={continent} key={continent}>
+            <input
+              type="checkbox"
+              name={`continent-${continent}`}
+              className="create__checkbox"
+              value={continent}
+              onChange={this.checkHandler}
+            />
+          </Label>
+        ))}
+        {this.state.errorMessage ? (
+          <p className="create__label-error">{this.state.errorMessage}</p>
+        ) : null}
       </div>
     );
   }
+
+  validate = (): boolean => {
+    if (this.choosenContinents.length === 0) {
+      this.setState({
+        errorMessage: 'Please select at least one continent',
+      });
+
+      return false;
+    }
+
+    this.setState({
+      errorMessage: '',
+    });
+
+    return true;
+  };
+
+  getValue = (): string[] => {
+    return this.choosenContinents;
+  };
 }
