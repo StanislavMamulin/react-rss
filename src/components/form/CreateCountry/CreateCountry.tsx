9addae2b184adb6e1f8044ad0c12aad1bee84f3f
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-27 00:47:19 +0500

    feat: create country card when click on create button

--- src/components/form/CreateCountry/CreateCountry.tsx
+++ src/components/form/CreateCountry/CreateCountry.tsx
@@ -8,19 +8,32 @@ import { SelectWeekStart } from './Fields/WeekStart';
 import { CountryName } from './Fields/CountryName';
 import { Capital } from './Fields/Capital';
 import { NationalDay } from './Fields/NationalDay';
+import { Country } from 'data/Countries.model';
 
 import './CreateCountry.scss';
 
-type CreateCountryProps = Record<string, never>;
+type CreateCountryProps = {
+  addCardHandler: (countries: Country) => void;
+};
 
 export class CreateCountry extends PureComponent<CreateCountryProps> {
-  fieldsRefs: RefObject<CountryName | LandLocked | Capital | NationalDay | ContinentChooser>[] = [];
+  fieldsRefs: RefObject<
+    | CountryName
+    | LandLocked
+    | Capital
+    | NationalDay
+    | ContinentChooser
+    | SelectWeekStart
+    | UploadFlag
+  >[] = [];
 
   countryNameComponent: RefObject<CountryName> = createRef();
   capitalComponent: RefObject<Capital> = createRef();
   nationalDayComponent: RefObject<NationalDay> = createRef();
   landlockedComponent: RefObject<LandLocked> = createRef();
   continentComponent: RefObject<ContinentChooser> = createRef();
+  weekStartComponent: RefObject<SelectWeekStart> = createRef();
+  flagUploadComponent: RefObject<UploadFlag> = createRef();
 
   constructor(props: CreateCountryProps) {
     super(props);
@@ -30,17 +43,33 @@ export class CreateCountry extends PureComponent<CreateCountryProps> {
     this.fieldsRefs.push(this.landlockedComponent);
     this.fieldsRefs.push(this.nationalDayComponent);
     this.fieldsRefs.push(this.continentComponent);
+    this.fieldsRefs.push(this.weekStartComponent);
+    this.fieldsRefs.push(this.flagUploadComponent);
   }
 
   handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    this.fieldsRefs.forEach((fieldRef) => fieldRef.current?.validate());
-
+    this.fieldsRefs.forEach((fieldRef) => {
+      fieldRef.current?.validate();
+    });
     const allOk = this.fieldsRefs.every((fieldRef) => fieldRef.current?.validate());
 
     const form = event.target as HTMLFormElement;
+
     if (allOk) {
+      this.props.addCardHandler({
+        name: {
+          common: this.countryNameComponent.current?.getValue(),
+        },
+        capital: [this.capitalComponent.current?.getValue()],
+        startOfWeek: this.weekStartComponent.current?.getValue(),
+        continents: this.continentComponent.current?.getValue(),
+        landlocked: this.landlockedComponent.current?.getValue(),
+        flags: {
+          png: this.flagUploadComponent.current?.getValue(),
+        },
+      });
       form.reset();
     }
   };
@@ -52,10 +81,10 @@ export class CreateCountry extends PureComponent<CreateCountryProps> {
           <CountryName ref={this.countryNameComponent} />
           <Capital ref={this.capitalComponent} />
           <NationalDay ref={this.nationalDayComponent} />
-          <SelectWeekStart />
+          <SelectWeekStart ref={this.weekStartComponent} />
           <ContinentChooser ref={this.continentComponent} />
           <LandLocked ref={this.landlockedComponent} />
-          <UploadFlag />
+          <UploadFlag ref={this.flagUploadComponent} />
 
           <input type="submit" value="Create" className="create__submit" />
         </form>
--- src/pages/Create/CreatePage.tsx
+++ src/pages/Create/CreatePage.tsx
@@ -1,13 +1,33 @@
 import { CreateCountry } from '../../components/form/CreateCountry/CreateCountry';
 import { PureComponent, ReactNode } from 'react';
+import { Country } from 'data/Countries.model';
+import { CardsList } from '../../components/ui/Cards/Cards';
+
 import './CreatePage.scss';
 
-export class CreatePage extends PureComponent {
+type CreatePageProps = Record<string, null>;
+
+type CreatePageState = {
+  fantasyCountries: Country[];
+};
+
+export class CreatePage extends PureComponent<CreatePageProps, CreatePageState> {
+  state: CreatePageState = {
+    fantasyCountries: [],
+  };
+
+  addCardHandler = (country: Country) => {
+    this.setState((prevState) => ({
+      fantasyCountries: [...prevState.fantasyCountries, country],
+    }));
+  };
+
   render(): ReactNode {
     return (
       <div className="create-page__container">
         <h1>Create your own fantasy world</h1>
-        <CreateCountry />
+        <CreateCountry addCardHandler={this.addCardHandler} />
+        <CardsList countries={this.state.fantasyCountries} />
       </div>
     );
   }
