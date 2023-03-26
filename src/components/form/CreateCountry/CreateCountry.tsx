2fafb34a23e62eb07493970bca01d10e6f5cafb5
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-26 20:50:03 +0500

    feat: add references to components

--- src/components/form/CreateCountry/CreateCountry.tsx
+++ src/components/form/CreateCountry/CreateCountry.tsx
@@ -1,5 +1,5 @@
-import { PureComponent, ReactNode, SyntheticEvent } from 'react';
-import { ContinentChooser } from './Continent';
+import { PureComponent, ReactNode, FormEvent, RefObject, createRef } from 'react';
+import { ContinentChooser } from './Fields/Continent';
 
 import { LandLocked } from './Fields/LandLocked';
 import { UploadFlag } from './Fields/UploadFlag';
@@ -11,8 +11,28 @@ import { NationalDay } from './Fields/NationalDay';
 
 import './CreateCountry.scss';
 
-export class CreateCountry extends PureComponent {
-  handleSubmit = (event: SyntheticEvent) => {
+type CreateCountryProps = Record<string, never>;
+
+export class CreateCountry extends PureComponent<CreateCountryProps> {
+  fieldsRefs: RefObject<CountryName | LandLocked | Capital | NationalDay | ContinentChooser>[] = [];
+
+  countryNameComponent: RefObject<CountryName> = createRef();
+  capitalComponent: RefObject<Capital> = createRef();
+  nationalDayComponent: RefObject<NationalDay> = createRef();
+  landlockedComponent: RefObject<LandLocked> = createRef();
+  continentComponent: RefObject<ContinentChooser> = createRef();
+
+  constructor(props: CreateCountryProps) {
+    super(props);
+
+    this.fieldsRefs.push(this.countryNameComponent);
+    this.fieldsRefs.push(this.capitalComponent);
+    this.fieldsRefs.push(this.landlockedComponent);
+    this.fieldsRefs.push(this.nationalDayComponent);
+    this.fieldsRefs.push(this.continentComponent);
+  }
+
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(event);
   };
@@ -21,12 +41,12 @@ export class CreateCountry extends PureComponent {
     return (
       <div className="create__container">
         <form className="create__form" onSubmit={this.handleSubmit}>
-          <CountryName />
-          <Capital />
-          <NationalDay />
+          <CountryName ref={this.countryNameComponent} />
+          <Capital ref={this.capitalComponent} />
+          <NationalDay ref={this.nationalDayComponent} />
           <SelectWeekStart />
-          <ContinentChooser />
-          <LandLocked />
+          <ContinentChooser ref={this.continentComponent} />
+          <LandLocked ref={this.landlockedComponent} />
           <UploadFlag />
 
           <input type="submit" value="Create" className="create__submit" />
