968bb43dccf3d4058f7989433ab4e14b6d626203
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-26 23:05:26 +0500

    feat: add getvalue method for week day component

--- src/components/form/CreateCountry/Fields/WeekStart.tsx
+++ src/components/form/CreateCountry/Fields/WeekStart.tsx
@@ -1,5 +1,6 @@
-import { PureComponent, ReactNode } from 'react';
+import { PureComponent, ReactNode, SyntheticEvent } from 'react';
 import { Label } from './Label';
+import { Field } from './fields.model';
 
 import '../CreateCountry.scss';
 
@@ -8,15 +9,29 @@ enum WeekStart {
   sunday = 'Sunday',
 }
 
-export class SelectWeekStart extends PureComponent {
+export class SelectWeekStart extends PureComponent implements Field<string> {
+  private weekStartDay = WeekStart.monday;
+
+  dayChangedHandler = (event: SyntheticEvent) => {
+    this.weekStartDay = (event.target as HTMLSelectElement).value as WeekStart;
+  };
+
   render(): ReactNode {
     return (
       <Label title="Start of week:">
-        <select name="start-of-week">
+        <select name="start-of-week" onChange={this.dayChangedHandler}>
           <option value={WeekStart.monday}>{WeekStart.monday}</option>
           <option value={WeekStart.sunday}>{WeekStart.sunday}</option>
         </select>
       </Label>
     );
   }
+
+  validate = (): boolean => {
+    return true;
+  };
+
+  getValue = (): string => {
+    return this.weekStartDay;
+  };
 }
