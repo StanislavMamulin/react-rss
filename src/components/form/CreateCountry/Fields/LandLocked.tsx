ff2378f3b243b8874bd4ed7616cd00c3ca3b10b4
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-27 01:30:24 +0500

    feat: show error if landlocked not chosen

--- src/components/form/CreateCountry/Fields/LandLocked.tsx
+++ src/components/form/CreateCountry/Fields/LandLocked.tsx
@@ -4,14 +4,28 @@ import { Field } from './fields.model';
 
 import '../CreateCountry.scss';
 
-export class LandLocked extends PureComponent implements Field<boolean> {
+type LandLockedProps = {
+  ref: RefObject<LandLocked>;
+};
+
+type LandLockedState = {
+  errorMessage: string;
+};
+
+export class LandLocked
+  extends PureComponent<LandLockedProps, LandLockedState>
+  implements Field<boolean>
+{
   landlockedInput: RefObject<HTMLInputElement> = createRef();
   notLandlockedInput: RefObject<HTMLInputElement> = createRef();
+  state = {
+    errorMessage: '',
+  };
 
   render(): ReactNode {
     return (
       <div className="create__landlocked-container">
-        <Label title="Landlocked:">
+        <Label title="Landlocked:" errorMessage={this.state.errorMessage}>
           <label className="create__label">
             <input type="radio" name="landlocked" value="yes" ref={this.landlockedInput} />
             Yes
@@ -26,9 +40,21 @@ export class LandLocked extends PureComponent implements Field<boolean> {
   }
 
   validate(): boolean {
-    return Boolean(
+    const isSomethingSelected = Boolean(
       this.landlockedInput.current?.checked || this.notLandlockedInput.current?.checked
     );
+
+    if (!isSomethingSelected) {
+      this.setState({
+        errorMessage: 'Please select one of the options',
+      });
+      return false;
+    }
+
+    this.setState({
+      errorMessage: '',
+    });
+    return true;
   }
 
   getValue(): boolean {
