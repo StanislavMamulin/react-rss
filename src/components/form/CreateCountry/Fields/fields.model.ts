441e40907b6c5e795f037f46505d33a06e042ea8
Stanislav Mamulin <StanislavPM@ya.ru>
2023-03-31 17:31:27 +0500

    feat: add type for props for my react hook form fields

--- src/components/form/CreateCountry/Fields/fields.model.ts
+++ src/components/form/CreateCountry/Fields/fields.model.ts
@@ -1,3 +1,10 @@
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+
+export type ReactHookFormFieldProps = {
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+};
+
 export interface Field<T> {
   validate: () => boolean;
   getValue: () => T;
