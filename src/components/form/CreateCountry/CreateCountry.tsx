9f0ddd26a55c504b0e188060b9c088dd8afa94da
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-01 17:49:22 +0500

    refactor: convert create country component to functional component with react hook form

--- src/components/form/CreateCountry/CreateCountry.tsx
+++ src/components/form/CreateCountry/CreateCountry.tsx
@@ -1,4 +1,6 @@
-import { FormEvent, RefObject, createRef, useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
 import { ContinentChooser } from './Fields/Continent';
 
 import { LandLocked } from './Fields/LandLocked';
@@ -11,6 +13,7 @@ import { NationalDay } from './Fields/NationalDay';
 import { Country } from 'data/Countries.model';
 
 import './CreateCountry.scss';
+import { FormValues } from './Fields/fields.model';
 
 type CreateCountryProps = {
   addCardHandler: (countries: Country) => void;
@@ -21,83 +24,54 @@ export const CreateCountry = ({
   addCardHandler,
   isCountryExist,
 }: CreateCountryProps): JSX.Element => {
-  const [fieldsRefs, setFieldsRefs] = useState<
-    RefObject<
-      | CountryName
-      | LandLocked
-      | Capital
-      | NationalDay
-      | ContinentChooser
-      | SelectWeekStart
-      | UploadFlag
-    >[]
-  >([]);
-
-  const countryNameComponent: RefObject<CountryName> = useRef(null);
-  const capitalComponent: RefObject<Capital> = useRef(null);
-  const nationalDayComponent: RefObject<NationalDay> = useRef(null);
-  const landlockedComponent: RefObject<LandLocked> = useRef(null);
-  const continentComponent: RefObject<ContinentChooser> = useRef(null);
-  const weekStartComponent: RefObject<SelectWeekStart> = useRef(null);
-  const flagUploadComponent: RefObject<UploadFlag> = useRef(null);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<FormValues>();
 
   useEffect(() => {
-    setFieldsRefs([
-      countryNameComponent,
-      capitalComponent,
-      landlockedComponent,
-      nationalDayComponent,
-      continentComponent,
-      weekStartComponent,
-      flagUploadComponent,
-    ]);
-  }, []);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    fieldsRefs.forEach((fieldRef) => {
-      fieldRef.current?.validate();
-    });
-    const allOk = fieldsRefs.every((fieldRef) => fieldRef.current?.validate());
-
-    const form = event.target as HTMLFormElement;
-
-    if (allOk) {
-      const newCountry = {
-        name: {
-          common: countryNameComponent.current?.getValue(),
-        },
-        capital: [capitalComponent.current?.getValue()],
-        nationalDay: nationalDayComponent.current?.getValue(),
-        startOfWeek: weekStartComponent.current?.getValue(),
-        continents: continentComponent.current?.getValue(),
-        landlocked: landlockedComponent.current?.getValue(),
-        flags: {
-          png: flagUploadComponent.current?.getValue(),
-        },
-      };
-
-      if (isCountryExist(newCountry)) {
-        return;
-      }
-
-      addCardHandler(newCountry);
-      form.reset();
-      continentComponent.current?.clear();
+    if (isSubmitSuccessful) {
+      reset();
     }
+  }, [isSubmitSuccessful, reset]);
+
+  const onSubmitHandle: SubmitHandler<FormValues> = (data) => {
+    const { coutryName, capital, nationalDay, startOfWeek, continents, landlocked, flagFile } =
+      data;
+
+    const newCountry: Country = {
+      name: {
+        common: coutryName,
+      },
+      capital: [capital],
+      nationalDay: nationalDay.toLocaleDateString(),
+      startOfWeek,
+      continents,
+      landlocked: landlocked === 'yes',
+      flags: {
+        png: URL.createObjectURL(flagFile[0]),
+      },
+    };
+
+    if (isCountryExist(newCountry)) {
+      return;
+    }
+
+    addCardHandler(newCountry);
   };
 
   return (
     <div className="create__container">
-      <form className="create__form" onSubmit={handleSubmit}>
-        <CountryName ref={countryNameComponent} />
-        <Capital ref={capitalComponent} />
-        <NationalDay ref={nationalDayComponent} />
-        <SelectWeekStart ref={weekStartComponent} />
-        <ContinentChooser ref={continentComponent} />
-        <LandLocked ref={landlockedComponent} />
-        <UploadFlag ref={flagUploadComponent} />
+      <form className="create__form" onSubmit={handleSubmit(onSubmitHandle)}>
+        <CountryName register={register} errors={errors} />
+        <Capital register={register} errors={errors} />
+        <NationalDay register={register} errors={errors} />
+        <SelectWeekStart register={register} errors={errors} />
+        <ContinentChooser register={register} errors={errors} />
+        <LandLocked register={register} errors={errors} />
+        <UploadFlag register={register} errors={errors} />
 
         <input type="submit" value="Create" className="create__submit" />
       </form>
--- src/components/form/CreateCountry/Fields/fields.model.ts
+++ src/components/form/CreateCountry/Fields/fields.model.ts
@@ -1,7 +1,7 @@
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
 
 export type ReactHookFormFieldProps = {
-  register: UseFormRegister<FieldValues>;
+  register: UseFormRegister<FormValues>;
   errors: FieldErrors<FieldValues>;
 };
 
@@ -9,3 +9,13 @@ export interface Field<T> {
   validate: () => boolean;
   getValue: () => T;
 }
+
+export type FormValues = {
+  capital: string;
+  continents: string[];
+  coutryName: string;
+  flagFile: FileList;
+  landlocked: string;
+  nationalDay: Date;
+  startOfWeek: string;
+};
