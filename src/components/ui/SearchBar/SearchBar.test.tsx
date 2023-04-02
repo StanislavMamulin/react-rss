bfaed0c606345458bdee46d127fda05428123e59
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-02 14:19:24 +0500

    test: add tests for search bar

---
+++ src/components/ui/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,36 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { SearchBar } from './SearchBar';
+
+describe('Search bar', () => {
+  beforeEach(() => {
+    render(<SearchBar />);
+  });
+
+  it('should display', () => {
+    expect(screen.queryByPlaceholderText('Search...')).toBeInTheDocument();
+  });
+
+  it('should cleared when clear button clicked', () => {
+    let clearButton;
+    const searchInput = screen.getByRole('textbox');
+    const searchValue = 'something';
+
+    clearButton = screen.queryByRole('button');
+    expect(clearButton).not.toBeInTheDocument();
+
+    fireEvent.input(searchInput, {
+      target: {
+        value: searchValue,
+      },
+    });
+
+    expect(screen.queryByDisplayValue(searchValue)).toBeInTheDocument();
+
+    clearButton = screen.queryByRole('button');
+    expect(clearButton).toBeInTheDocument();
+
+    fireEvent.mouseDown(clearButton!);
+    expect(screen.queryByDisplayValue(searchValue)).not.toBeInTheDocument();
+  });
+});
