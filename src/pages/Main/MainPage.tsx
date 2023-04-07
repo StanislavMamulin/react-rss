b46998f26dc69d6a1590957048b43c890026be5a
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-07 19:12:44 +0500

    feat: add getting of countries from server in main page

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -2,21 +2,42 @@ import { SearchBar } from '../../components/ui/SearchBar/SearchBar';
 import { Cards } from '../../components/ui/Cards/Cards';
 import { useEffect, useState } from 'react';
 import { Country } from '../../data/Countries.model';
-import { getCountries } from '../../components/ui/Cards/Cards.model';
+import { getAllCountries } from '../../components/ui/Cards/Cards.model';
 import './MainPage.scss';
 
 export const MainPage = (): JSX.Element => {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const requestCountries = async (controller: AbortController) => {
+    try {
+      const countries: Country[] = await getAllCountries(controller);
+      setCountries(countries);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
-    const countries: Country[] = getCountries();
-    setCountries(countries);
+    const controller = new AbortController();
+    requestCountries(controller);
+
+    return () => {
+      controller.abort();
+    };
   }, []);
+
+  const searchHandler = (searchText: string): void => {
+    setSearchValue(searchText);
+  };
 
   return (
     <div className="main-page__container">
-      <SearchBar />
-      <Cards countries={countries} />
+      <SearchBar searchSubmit={searchHandler} />
+      <Cards countries={countries} isLoading={isLoading} />
     </div>
   );
 };
