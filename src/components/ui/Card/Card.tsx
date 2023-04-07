fb6a8332058ec2762928ef92219cd52c20992c3e
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-07 21:51:15 +0500

    refactor: convert card to functional component

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,19 +1,16 @@
 import { Country } from 'data/Countries.model';
-import { Component } from 'react';
 import { CardProps } from './Card.model';
 import { CardImage } from './CardImage/CardImage';
 import './Card.scss';
 import { CardInfo } from './CardInfo/CardInfo';
 
-export class Card extends Component<CardProps> {
-  render() {
-    const { flag, flags, ...countryInfo }: Country = this.props.country;
+export const Card = ({ country }: CardProps) => {
+  const { flag, flags, ...countryInfo }: Country = country;
 
-    return (
-      <div className="card-wrapper">
-        <CardImage flagEmoji={flag} flagImage={flags?.png} />
-        <CardInfo countryInfo={countryInfo} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-wrapper">
+      <CardImage flagEmoji={flag} flagImage={flags?.png} />
+      <CardInfo countryInfo={countryInfo} />
+    </div>
+  );
+};
