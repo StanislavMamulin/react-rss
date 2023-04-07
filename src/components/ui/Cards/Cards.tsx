6495be57f6d127cf6403050e45bfc844534db5b7
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-07 20:32:25 +0500

    feat: display loader in cards component when data loading

--- src/components/ui/Cards/Cards.tsx
+++ src/components/ui/Cards/Cards.tsx
@@ -2,6 +2,7 @@ import { Card } from '../Card/Card';
 import { Country } from 'data/Countries.model';
 import { CardsListProps, CardsProps } from './Cards.model';
 import './Cards.scss';
+import { Loader } from '../Loader/Loader';
 
 export const CardsList = ({ countries }: CardsListProps): JSX.Element => {
   const listCountries: JSX.Element[] = countries.map((countryItem: Country) => (
@@ -11,10 +12,17 @@ export const CardsList = ({ countries }: CardsListProps): JSX.Element => {
   return <>{listCountries}</>;
 };
 
-export const Cards = ({ countries }: CardsProps) => {
+const NothingFound = (): JSX.Element => (
+  <>
+    <h3>Nothing found</h3>
+  </>
+);
+
+export const Cards = ({ countries, isLoading }: CardsProps) => {
   return (
     <div className="cards-wrapper">
-      <CardsList countries={countries} />
+      {countries.length === 0 ? <NothingFound /> : <CardsList countries={countries} />}
+      {isLoading && <Loader />}
     </div>
   );
 };
