6c6173553fed22ecc29a5cee54d3ad4cc1b9e4bb
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-08 23:23:43 +0500

    feat: implemented search for movies by name

--- src/services/movieAPI.ts
+++ src/services/movieAPI.ts
@@ -2,7 +2,9 @@ import { MovieMainInfo, MovieResponses } from '../data/Movies.model';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
-const POPULAR_MOVIE_URL = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+const DEFAULT_PARAMS = `api_key=${API_KEY}&language=en-US`;
+const POPULAR_MOVIE_URL = `${BASE_URL}/movie/popular?${DEFAULT_PARAMS}`;
+const SEARCH_URL = `${BASE_URL}/search/movie?${DEFAULT_PARAMS}&include_adult=false`;
 
 export const getPopularMovies = async (
   controller: AbortController,
@@ -10,6 +12,27 @@ export const getPopularMovies = async (
 ): Promise<MovieMainInfo[]> => {
   try {
     const response = await fetch(`${POPULAR_MOVIE_URL}&page=${page}`, {
+      signal: controller.signal,
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const movies: MovieResponses = await response.json();
+
+    return movies.results;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+export const searchMovieByName = async (
+  searchText: string,
+  controller: AbortController
+): Promise<MovieMainInfo[]> => {
+  try {
+    const response = await fetch(`${SEARCH_URL}&query=${searchText}`, {
       signal: controller.signal,
     });
     if (!response.ok) {
