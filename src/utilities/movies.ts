8a609d30d9f0689569a069b47e6a67023775e537
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-08 20:20:45 +0500

    feat: add movie card component

---
+++ src/utilities/movies.ts
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://image.tmdb.org/t/p/';
+const backdropSizes = ['w300', 'w780', 'w1280', 'original'];
+const posterSizes = ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'];
+
+export const getFullPosterPath = (pathPart: string): string =>
+  `${BASE_URL}${posterSizes[4]}${pathPart}`;
+
+export const getFullBackdropPath = (pathPart: string): string =>
+  `${BASE_URL}${backdropSizes[2]}${pathPart}`;
