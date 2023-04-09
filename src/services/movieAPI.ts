b5e3aab17ab3c329f9517ab960332e858c0ec935
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-09 10:21:24 +0500

    feat: add function to get movie details

--- src/services/movieAPI.ts
+++ src/services/movieAPI.ts
@@ -1,9 +1,10 @@
-import { MovieMainInfo, MovieResponses } from '../data/Movies.model';
+import { MovieDetails, MovieMainInfo, MovieResponses } from '../data/Movies.model';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 const DEFAULT_PARAMS = `api_key=${API_KEY}&language=en-US`;
-const POPULAR_MOVIE_URL = `${BASE_URL}/movie/popular?${DEFAULT_PARAMS}`;
+const MOVIE_REQUEST_URL = `${BASE_URL}/movie`;
+const POPULAR_MOVIE_URL = `${MOVIE_REQUEST_URL}/popular?${DEFAULT_PARAMS}`;
 const SEARCH_URL = `${BASE_URL}/search/movie?${DEFAULT_PARAMS}&include_adult=false`;
 
 export const getPopularMovies = async (
@@ -42,6 +43,29 @@ export const searchMovieByName = async (
     const movies: MovieResponses = await response.json();
 
     return movies.results;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+export const getMovieDetailsById = async (
+  id: number,
+  controller: AbortController
+): Promise<MovieDetails> => {
+  try {
+    const getDetailUrl = `${MOVIE_REQUEST_URL}/${id}?${DEFAULT_PARAMS}`;
+
+    const response: Response = await fetch(getDetailUrl, {
+      signal: controller.signal,
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const movieDetails: MovieDetails = await response.json();
+
+    return movieDetails;
   } catch (err) {
     console.error(err);
     throw err;
