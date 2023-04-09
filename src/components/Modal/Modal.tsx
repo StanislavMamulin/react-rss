6c56ff9021a650c1f01abd7b29b99f026d848bd9
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-09 17:32:08 +0500

    refactor: rewrite close modal function

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent, cloneElement, Children } from 'react';
+import { cloneElement, Children, useRef, MouseEvent } from 'react';
 import './Modal.scss';
 
 type ModalProps = {
@@ -7,16 +7,16 @@ type ModalProps = {
 };
 
 export const Modal = ({ children, onClose }: ModalProps) => {
-  const backgroundClickHandler = (event: SyntheticEvent) => {
-    if (event.target instanceof HTMLElement) {
-      if (event.target.parentElement === document.body) {
-        onClose();
-      }
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const backgroundClickHandler = (event: MouseEvent<HTMLDivElement>) => {
+    if (modalRef.current && modalRef.current === event.target) {
+      onClose();
     }
   };
 
   return (
-    <div className="modal" onClick={backgroundClickHandler}>
+    <div className="modal" onClick={backgroundClickHandler} ref={modalRef}>
       {Children.map(children, (child) => {
         return cloneElement(child, { onClose });
       })}
