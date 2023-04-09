75a11adea77763756d91c83233b02799437f2da1
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-10 03:01:17 +0500

    test: fix test for new props of searchbar component

--- src/components/ui/SearchBar/SearchBar.test.tsx
+++ src/components/ui/SearchBar/SearchBar.test.tsx
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { SearchBar } from './SearchBar';
 
 describe('Search bar', () => {
   beforeEach(() => {
-    render(<SearchBar />);
+    render(<SearchBar searchSubmit={vi.fn()} />);
   });
 
   it('should display', () => {
