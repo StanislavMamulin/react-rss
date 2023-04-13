9bac956693095a2c5b8d33a0be99f2434a261e17
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-13 16:41:12 +0500

    feat: init store and add it to the app module

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import { worker } from './mocks/browser';
 import App from './App';
+import { store } from './redux/store';
 import './index.css';
 
 if (process.env.NODE_ENV === 'development') {
@@ -12,7 +15,9 @@ if (process.env.NODE_ENV === 'development') {
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
