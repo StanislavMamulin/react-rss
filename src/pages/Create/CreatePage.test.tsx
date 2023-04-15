900ff76ffff127017fd8b6cb23f835974ad5c6b5
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-15 20:20:47 +0500

    test: create test for case of duplicate country name

--- src/pages/Create/CreatePage.test.tsx
+++ src/pages/Create/CreatePage.test.tsx
@@ -2,6 +2,33 @@ import { describe, it, expect, vi } from 'vitest';
 import { render, screen, fireEvent, waitFor } from '../../utilities/test-utils';
 import { CreatePage } from './CreatePage';
 import userEvent from '@testing-library/user-event';
+import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
+
+const createTestCountry = async (countryName: string, user: UserEvent) => {
+  const countryInput = screen.getByRole('textbox', { name: /^country/i });
+  fireEvent.input(countryInput, {
+    target: {
+      value: countryName,
+    },
+  });
+
+  const capitalInput = screen.getByRole('textbox', { name: /^capital/i });
+  userEvent.clear(capitalInput);
+  await userEvent.type(capitalInput, 'Kigartun');
+
+  fireEvent.change(screen.getByLabelText(/national day/i), { target: { value: '1888-05-24' } });
+
+  fireEvent.change(screen.getByLabelText(/Start of week/i), { target: { value: 'Monday' } });
+
+  fireEvent.click(screen.getByRole('checkbox', { name: /^hazos/i }));
+
+  fireEvent.click(screen.getByRole('radio', { name: /^no/i }));
+
+  // flag image upload
+  const uploader = screen.getByLabelText(/flag image/i);
+  const testFile = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
+  await user.upload(uploader, testFile);
+};
 
 describe('Create country', () => {
   it('should display an error when there are no required values in the fields', async () => {
@@ -32,37 +59,32 @@ describe('Create country', () => {
     render(<CreatePage />);
 
     const TEST_COUNTRY_NAME = 'Nuimbur';
-
-    fireEvent.input(screen.getByRole('textbox', { name: /^country/i }), {
-      target: {
-        value: TEST_COUNTRY_NAME,
-      },
-    });
-
-    fireEvent.input(screen.getByRole('textbox', { name: /^capital/i }), {
-      target: {
-        value: 'Kigartun',
-      },
-    });
-
-    fireEvent.change(screen.getByLabelText(/national day/i), { target: { value: '1888-05-24' } });
-
-    fireEvent.change(screen.getByLabelText(/Start of week/i), { target: { value: 'Monday' } });
-
-    fireEvent.click(screen.getByRole('checkbox', { name: /^hazos/i }));
-
-    fireEvent.click(screen.getByRole('radio', { name: /^no/i }));
-
-    // flag image upload
-    const uploader = screen.getByLabelText(/flag image/i);
-    const testFile = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
-    await user.upload(uploader, testFile);
-
     window.URL.createObjectURL = vi.fn(); // URL.createObjectURL doesn't exist in test environment and you need to make a stub
 
+    await createTestCountry(TEST_COUNTRY_NAME, user);
+    await waitFor(() => fireEvent.submit(screen.getByRole('button', { name: 'Create' })));
+
+    await createTestCountry(TEST_COUNTRY_NAME, user);
     await waitFor(() => fireEvent.submit(screen.getByRole('button', { name: 'Create' })));
 
     const newCountryNameElement = screen.getByText(TEST_COUNTRY_NAME);
     expect(newCountryNameElement).toBeInTheDocument();
+  });
+
+  it('should not create dublicate of a country with same name', async () => {
+    const user = userEvent.setup();
+    render(<CreatePage />);
+
+    const TEST_COUNTRY_NAME = 'NuimburArc';
+    window.URL.createObjectURL = vi.fn(); // URL.createObjectURL doesn't exist in test environment and you need to make a stub
+
+    await createTestCountry(TEST_COUNTRY_NAME, user);
+    await waitFor(() => fireEvent.submit(screen.getByRole('button', { name: 'Create' })));
+
+    await createTestCountry(TEST_COUNTRY_NAME, user);
+    await waitFor(() => fireEvent.submit(screen.getByRole('button', { name: 'Create' })));
+
+    const countryExist = await screen.findByText(/exists/i);
+    expect(countryExist).toBeInTheDocument();
   });
 });
