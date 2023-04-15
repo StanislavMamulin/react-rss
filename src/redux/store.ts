8688a7e34e664b6f056abf61ace486d4daddb93b
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-15 13:31:43 +0500

    test: setup redux store config and create wrapper for ability to make tests with redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 import countriesReducer from './createCountrySlice';
 import moviesReducer from './movieSlice';
 import { movieApi } from '../services/movieAPI';
 
-export const store = configureStore({
-  reducer: {
-    countries: countriesReducer,
-    movies: moviesReducer,
-    [movieApi.reducerPath]: movieApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(movieApi.middleware),
+const rootReducer = combineReducers({
+  countries: countriesReducer,
+  movies: moviesReducer,
+  [movieApi.reducerPath]: movieApi.reducer,
 });
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(movieApi.middleware),
+    preloadedState,
+  });
+
+export const store = setupStore();
 
 setupListeners(store.dispatch);
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof setupStore>;
