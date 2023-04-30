0203285124429551c9c9e78d85ebf7fd7fcab708
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-01 02:02:52 +0500

    test(e2e): add main page tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,27 @@
 describe('Main page e2e test', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
   it('should load the main page', () => {
-    cy.visit('http://localhost:3001');
+    cy.contains('Popular movies');
+  });
+
+  it('should do search', () => {
+    cy.get('input').type('lord{enter}');
+
+    cy.get('.card-wrapper').should('have.length', 20);
+  });
+
+  it('should show movie details', () => {
+    cy.contains('Avatar').click();
+    cy.contains('Release date: 2002-12-18').should('exist');
+  });
+
+  it('should close modal window', () => {
+    cy.contains('Avatar').click();
+    cy.contains('Release date: 2002-12-18').should('exist');
+    cy.get('button').contains('✖').click();
+    cy.contains('Release date: 2002-12-18').should('not.exist');
   });
 });
