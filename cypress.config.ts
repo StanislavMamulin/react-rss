b345b614af336e2a7724541c0e0545062bbd3eb3
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-30 15:17:04 +0500

    test: setup cypress code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'cypress';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: ['cypress/**/*.*', 'instrumented/**/*.*'],
+      url: 'http://localhost:3001/__coverage__',
+    },
+  },
   e2e: {
+    baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config);
+
+      return config;
     },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,44 @@
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      include: 'src/**/*.{ts,tsx}',
+      exclude: ['**/node_modules/**', '**/public/**', '**/mocks/**', '**/instrumented/**'],
+      cypress: true,
+      requireEnv: true,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    exclude: [
+      ...configDefaults.exclude,
+      '**/public/**',
+      '**/mocks/**',
+      'src/main.tsx',
+      '**/instrumented/**',
+    ],
     coverage: {
       provider: 'istanbul',
       all: true,
-      exclude: [...configDefaults.exclude, '**/public/**', '**/mocks/**', 'src/main.tsx'],
+      exclude: [
+        ...configDefaults.exclude,
+        '**/public/**',
+        '**/mocks/**',
+        'src/main.tsx',
+        '**/instrumented/**',
+      ],
     },
+  },
+  server: {
+    host: true,
+    port: 3001,
   },
 });
