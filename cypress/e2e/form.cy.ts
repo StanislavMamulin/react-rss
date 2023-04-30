6a7301c0081918dff164f62167b798e397ccf319
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-01 02:03:15 +0500

    test(e2e): add form page tests

--- cypress/e2e/form.cy.ts
+++ cypress/e2e/form.cy.ts
@@ -18,9 +18,27 @@ describe('create countries e2e  test', () => {
       cy.get('input[type="file"]').selectFile('./src/mocks/mockData/mockMoviePoster.jpg');
 
       cy.get('form').contains('Create').click();
+      cy.get('form').contains(nowhereCountry.name).should('not.exist');
     });
 
     cy.contains(nowhereCountry.name).should('exist');
     cy.get('.card-wrapper').should('have.length', 2);
+  });
+  it('should not create a country with identical name', () => {
+    cy.visit('/create');
+
+    for (let i = 0; i < 2; i++) {
+      cy.get('form').contains('Country name').type(nowhereCountry.name);
+      cy.get('form').contains('Capital').type(nowhereCountry.capital);
+      cy.get('form').contains('National day').type(nowhereCountry.day);
+      cy.get('select').select(nowhereCountry.startOfWeek);
+      cy.contains(nowhereCountry.continent).click();
+      cy.contains(nowhereCountry.landlocked).click();
+      cy.get('input[type="file"]').selectFile('./src/mocks/mockData/mockMoviePoster.jpg');
+
+      cy.get('form').contains('Create').click();
+    }
+
+    cy.contains('A country with this name already exists, please introduce a new').should('exist');
   });
 });
